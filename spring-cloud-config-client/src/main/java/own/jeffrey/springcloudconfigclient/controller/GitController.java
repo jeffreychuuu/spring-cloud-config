4291b4fc0b3aee34172cf14be3497156f1b3f939
Jeffrey Chu <jeffreychu888hk@gmail.com>
2022-09-17 16:36:30 +0800

    add demo to get by @Value with @RefreshScope

--- spring-cloud-config-client/src/main/java/own/jeffrey/springcloudconfigclient/controller/GitController.java
+++ spring-cloud-config-client/src/main/java/own/jeffrey/springcloudconfigclient/controller/GitController.java
@@ -1,17 +1,28 @@
 package own.jeffrey.springcloudconfigclient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import own.jeffrey.springcloudconfigclient.component.ConfigurationProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class GitController {
     @Autowired
     ConfigurationProperty configurationProperty;
 
+    @Value("${application.user.name}")
+    private String name;
+
     @GetMapping(value = "/whoami")
     public ConfigurationProperty.User whoami() {
         return configurationProperty.getUser();
+    }
+
+    @GetMapping(value = "/name")
+    public String getName() {
+        return name;
     }
 }
